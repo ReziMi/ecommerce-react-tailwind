d2cae0a5b058fcaef287d881ef0b923b8264dc63
ReziMi <revaz.mikadze92@gmail.com>
2024-06-19 15:01:39 +0400

    best seller products section added with swiper

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from './Banner'
 import Category from './Category'
 import Products from './Products'
 import Collections from './Collections'
+import BestSeller from './BestSeller'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <Category/>
         <Products/>
         <Collections/>
+        <BestSeller/>
     </div>
   )
 }
