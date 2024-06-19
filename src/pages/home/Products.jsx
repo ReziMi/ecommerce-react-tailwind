4ba78806413d5a28ef2fb37a3392bf85a9c93907
ReziMi <revaz.mikadze92@gmail.com>
2024-06-19 14:14:20 +0400

    Products section with filtering and sorting functionalities added

--- src/pages/home/Products.jsx
+++ src/pages/home/Products.jsx
@@ -3,35 +3,81 @@ import { FaFilter } from "react-icons/fa";
 import Cards from "../../components/Cards";
 
 const Products = () => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [sortOption, setSortOption] = useState("default");
 
-    useEffect(()=>{
-        const fetchData = async () =>{
-            try{
-                const response = await fetch("/products.json");
-                const data = await response.json();
-                setProducts(data)
-            }catch(error){
-                console.log("Error fetching data:", error)
-            }
-        }
-        fetchData()
-    },[])
-    console.log(products)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/products.json");
+        const data = await response.json();
+        setProducts(data);
+        setFilteredItems(data);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // filtering function
+  const filterItems = (category) => {
+    const filtered =
+      category === "all"
+        ? products
+        : products.filter((item) => item.category === category);
+
+    setFilteredItems(filtered);
+    setSelectedCategory(category);
+  };
+
+  // show all products
+  const showAll = () => {
+    setFilteredItems(products);
+    setSelectedCategory("all");
+  };
+
+  // sorting functionality
+  const handleSortChange = (option) => {
+    setSortOption(option);
+
+    // logic for sorting
+    let sortedItems = [...filteredItems];
+
+    switch (option) {
+      case "A-Z":
+        sortedItems.sort((a, b) => a.title.localeCompare(b.title));
+        break;
+      case "Z-A":
+        sortedItems.sort((a, b) => b.title.localeCompare(a.title));
+        break;
+      case "low-to-high":
+        sortedItems.sort((a, b) => a.price - b.price);
+        break;
+      case "high-to-low":
+        sortedItems.sort((a, b) => b.price - a.price);
+        break;
+      default:
+        break;
+    }
+    setFilteredItems(sortedItems);
+  };
 
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-28 px-4 mb-12">
-      <h2 className="title">Or subscribe to the newsletter </h2>
+      <h2 className="title">Our Products</h2>
 
       {/* product cards */}
       <div>
         <div className="flex flex-col md:flex-row flex-wrap md:justify-between items-center space-y-3 mb-8">
           {/* all buttons */}
           <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap">
-            <button>All products</button>
-            <button>Clothing</button>
-            <button>Hoodies</button>
-            <button>Bags</button>
+            <button onClick={showAll}>All products</button>
+            <button onClick={() => filterItems("Dress")}>Clothing</button>
+            <button onClick={() => filterItems("Hoodies")}>Hoodies</button>
+            <button onClick={() => filterItems("Bag")}>Bags</button>
           </div>
 
           {/* sorting options */}
@@ -39,8 +85,14 @@ const Products = () => {
             <div className="bg-black p-2">
               <FaFilter className="text-white h-4 w-4" />
             </div>
-            <select className="bg-black text-white px-2 py-1 rounded-sm">
-              <option value="Default">Default</option>
+
+            <select
+              id="sort"
+              onChange={(e) => handleSortChange(e.target.value)}
+              value={sortOption}
+              className="bg-black text-white px-2 py-1 rounded-sm"
+            >
+              <option value="default">Default</option>
               <option value="A-Z">A-Z</option>
               <option value="Z-A">Z-A</option>
               <option value="low-to-high">Low to High</option>
@@ -49,8 +101,7 @@ const Products = () => {
           </div>
         </div>
 
-        <Cards filteredItems={products}/>
-
+        <Cards filteredItems={filteredItems} />
       </div>
     </div>
   );
